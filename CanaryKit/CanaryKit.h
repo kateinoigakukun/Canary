a582b8f10345848d3782164be4d4ef2c30c0d10e
Yuta Saito <kateinoigakukun@gmail.com>
2018-08-23 11:08:08 +0900

    Add CanaryKit

---
+++ CanaryKit/CanaryKit.h
@@ -0,0 +1,19 @@
+//
+//  CanaryKit.h
+//  CanaryKit
+//
+//  Created by Yuta Saito on 2018/08/23.
+//  Copyright © 2018年 bangohan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CanaryKit.
+FOUNDATION_EXPORT double CanaryKitVersionNumber;
+
+//! Project version string for CanaryKit.
+FOUNDATION_EXPORT const unsigned char CanaryKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CanaryKit/PublicHeader.h>
+
+
