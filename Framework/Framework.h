d16860682de3658f53616178c589ea0886a6d307
Yuta Saito <kateinoigakukun@gmail.com>
2018-08-23 11:12:45 +0900

    Add Framework

---
+++ Framework/Framework.h
@@ -0,0 +1,19 @@
+//
+//  Framework.h
+//  Framework
+//
+//  Created by Yuta Saito on 2018/08/23.
+//  Copyright © 2018年 bangohan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Framework.
+FOUNDATION_EXPORT double FrameworkVersionNumber;
+
+//! Project version string for Framework.
+FOUNDATION_EXPORT const unsigned char FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Framework/PublicHeader.h>
+
+
